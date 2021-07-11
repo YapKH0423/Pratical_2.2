373ef21fd36fa2937e10f238e181aea40448d2ec
YapKH0423 <yapkh-wm20@student.tarc.edu.my>
2021-07-12 04:28:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/pratical_2_2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pratical_2_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mycompany.aboutme.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
